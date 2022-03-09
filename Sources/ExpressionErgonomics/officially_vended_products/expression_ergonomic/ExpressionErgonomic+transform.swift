a870e8d94aaf1fc9491b662defd6e769cceac413
Jeremy Bannister <jeremy.a.bannister@gmail.com>
2022-03-09 12:26:33 +0100

    Added an async overload of `ExpressionErgonomic.transform(_:)`.

--- Sources/ExpressionErgonomics/officially_vended_products/expression_ergonomic/ExpressionErgonomic+transform.swift
+++ Sources/ExpressionErgonomics/officially_vended_products/expression_ergonomic/ExpressionErgonomic+transform.swift
@@ -6,10 +6,30 @@
 //
 
 ///
+@available(iOS 13.0, macOS 10.15.0, watchOS 6, tvOS 13, *)
 public extension ExpressionErgonomic {
     
     ///
-    func transform <NewValue> (_ transform: (Self)throws->NewValue) rethrows -> NewValue {
+    func transform
+        <NewValue>
+        (_ transform: (Self)async throws->NewValue)
+    async rethrows -> NewValue {
+        
+        ///
+        try await transform(self)
+    }
+}
+
+///
+public extension ExpressionErgonomic {
+    
+    ///
+    func transform
+        <NewValue>
+        (_ transform: (Self)throws->NewValue)
+    rethrows -> NewValue {
+        
+        ///
         try transform(self)
     }
 }
