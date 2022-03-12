ab3a08560866525fc9d0f113ab974baa9dcb72ec
Jeremy Bannister <jeremy.a.bannister@gmail.com>
2022-03-12 18:38:08 +0100

    Added `Set.inserting(_:)` and `Set.removing(_:)`.

---
+++ Sources/ExpressionErgonomics/officially_vended_products/_package_extensions/Foundation/Set+extensions.swift
@@ -0,0 +1,20 @@
+//
+//  Set+extensions.swift
+//  
+//
+//  Created by Jeremy Bannister on 3/12/22.
+//
+
+///
+public extension Set {
+    
+    /// This method returns this set after inserting the given member into it.
+    func inserting (_ newMember: Element) -> Self {
+        self.mutated { $0.insert(newMember) }
+    }
+    
+    /// This method returns this set after removing the given member from it.
+    func removing (_ member: Element) -> Self {
+        self.mutated { $0.remove(member) }
+    }
+}
