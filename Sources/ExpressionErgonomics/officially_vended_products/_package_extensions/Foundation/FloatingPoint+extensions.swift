9007bb82ef25fda3d8cf6454f91a69934b01a9c6
Jeremy Bannister <jeremy.a.bannister@gmail.com>
2023-08-14 01:07:43 +0200

    Added `FloatingPoint.divided(by:)`.

---
+++ Sources/ExpressionErgonomics/officially_vended_products/_package_extensions/Foundation/FloatingPoint+extensions.swift
@@ -0,0 +1,15 @@
+//
+//  FloatingPoint+extensions.swift
+//  
+//
+//  Created by Jeremy Bannister on 8/14/23.
+//
+
+///
+extension FloatingPoint {
+    
+    ///
+    public func divided (by divisor: Self) -> Self {
+        self / divisor
+    }
+}
